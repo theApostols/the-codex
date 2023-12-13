d2017f2f31b3e1db6439a80e9a45b788654eaad8
Stavros Panagiotopoulos <stavros_pana@hotmail.com>
2023-12-12 21:21:50 -0500

    started the resolvers, testing queries still WIP big time.

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,9 +1,59 @@
 // const typeDefs = ``;
 
+// const typeDefs = `
+//   type Query {
+//     hello: String
+//   }
+// `;
+
 const typeDefs = `
-  type Query {
-    hello: String
-  }
+type User {
+  id: ID!
+  username: String!
+  email: String!
+  snippets: [Snippet]
+  # need to add more fields WIP
+}
+
+type Snippet {
+  id: ID!
+  username: String!
+  snippetText: String!
+  comments: [Comment]
+  # need to add more fields WIP
+}
+
+type Comment {
+  id: ID!
+  username: String!
+  commentText: String!
+  # need to add more fields WIP
+}
+
+type CodeBlock {
+  id: ID!
+  username: String!
+  codeBlockText: String!
+  # need to add more fields WIP
+}
+
+type Query {
+  getUser(username: String!): User
+  getSnippet(id: ID!): Snippet
+  getComment(id: ID!): Comment
+  getCodeBlock(id: ID!): CodeBlock
+  # need to add more fields WIP
+}
+
+type Mutation {
+  createUser(username: String!, email: String!, password: String!): User
+  createSnippet(username: String!, snippetText: String!): Snippet
+  createComment(username: String!, commentText: String!): Comment
+  createCodeBlock(username: String!, codeBlockText: String!): CodeBlock
+  # need to add more fields WIP
+}
 `;
+
+
 
 module.exports = typeDefs;
