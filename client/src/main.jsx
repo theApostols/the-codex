1c01e8162a3c9eaa72456d269a32222b061c6860
Milena <132115087+milena-allaway@users.noreply.github.com>
2023-12-12 21:36:49 -0500

    imported Chakra UI in main.jsx and installed npm dependency

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { ChakraProvider } from "@chakra-ui/react";
 
 import "./index.css";
 
