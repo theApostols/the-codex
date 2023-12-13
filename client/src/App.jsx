294ef3d46df64253169d9a9ef66cd7873ddb5b24
Milena <132115087+milena-allaway@users.noreply.github.com>
2023-12-13 00:56:25 -0500

    feature/footer: created footer with email link. adjusted some css settings to make things work better for now. added icon for header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import Header from './components/Header/Header';
-// import Footer from './components/Footer';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <> 
         <Header />
         <Outlet />
-        {/* <Footer /> */}
+        <Footer />
     
     </>
   );
