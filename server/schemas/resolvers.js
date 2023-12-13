373b14c0abfb950ea3cd6873c04fccad2ec4f7f3
Stavros Panagiotopoulos <stavros_pana@hotmail.com>
2023-12-12 20:40:07 -0500

    STAVROS BRANCH

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,15 +1,85 @@
-// const {} = require("../models");
-// const {} = require("../utils/auth");
-
-// const resolvers = {
-//   Query: {},
-//   Mutation: {},
-// };
+const { User } = require("../models");
+const { signToken, AuthenticationError } = require("../utils/auth");
 
 const resolvers = {
   Query: {
-    hello: () => "Hello, world!",
+    users: async () => {
+      return User.find();
+    },
+   users: async (parent, { userId }) => {
+      return User.findOne({ _id: userId });
+    },
+    //Retrieve the logged in user without specifically searching for them
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id });
+      }
+      throw AuthenticationError;
+    },
+  },
+
+
+  Mutation: {
+    addUser: async (parent, { name, email, password }) => {
+      const user = await User.create({ name, email, password });
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw AuthenticationError;
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if(!correctPw) {
+        throw AuthenticationError;
+      }
+
+      const token = signToken(user);
+      return { token, user };
+  },
+
+  // Add a third argument to the resolver to access data in our context
+
+  addSkill: async (parent, { userId, skill }, context) => {
+    // If context has a `user` property, that means the user executing this mutation has a valid JWT and is logged in
+    if (context.user) {
+      return User.findOneAndUpdate(
+        { _id: userId },
+        {
+          $addToSet: { skills: skill },
+        },
+        {
+          new: true,
+          runValidators: true,
+        }
+      );
+    }
+    throw new AuthenticationError('You need to be logged in!');
+  },
+
+  // Set up mutation so a logged in user can only remove their profile and no one else's
+
+  removeUser: async (parent, args, context) => {
+    if (context.user) {
+      return User.findOneAndDelete({ _id: context.user._id });
+    }
+    throw new AuthenticationError('You need to be logged in!');
+  },
   },
 };
+
+
+
+
+// const resolvers = {
+//   Query: {
+//     hello: () => "Hello, world!",
+//   },
+// };
 
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,9 +1,40 @@
 // const typeDefs = ``;
 
+// const typeDefs = `
+//   type Query {
+//     hello: String
+//   }
+// `;
+
+
 const typeDefs = `
+  type User {
+    _id: ID!
+    name: String!
+    email: String!
+    password: String! 
+    skills: [String] 
+  }
+
+  type AuthPayload {
+    token: String
+    user: User
+  }
+
   type Query {
-    hello: String
+    users: [User]
+    user(userId: ID!): User
+    me: User
+  }
+
+  type Mutation {
+    addUser(name: String!, email: String!, password: String!): AuthPayload
+    login(email: String!, password: String!): AuthPayload
+    addSkill(userId: ID!, skill: String!): User
+    removeUser: User
   }
 `;
+
+
 
 module.exports = typeDefs;
