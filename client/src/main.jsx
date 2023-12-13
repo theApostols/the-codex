99f40c18ebd718ad6c829fe4a0fc27c9fd0676e5
Milena <132115087+milena-allaway@users.noreply.github.com>
2023-12-12 20:45:04 -0500

    made default home page, commented out unused imports for now

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,6 +6,7 @@ import "./index.css";
 
 // import pages that router will use
 import App from "./App.jsx";
+import Home from "./pages/Home.jsx";
 import ErrorPage from "./pages/ErrorPage.jsx";
 
 // routes and corresponding components
