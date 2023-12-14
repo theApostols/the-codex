fad10306e0b9f0581d663927011f59223e92a9bd
ethan <hertlethan@gmail.com>
2023-12-14 16:57:32 -0500

    updated resolvers & typedefs with main snippet queries

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,146 +1,155 @@
-const { User, Snippet, Comment, CodeBlock } = require("../models");
+const {User, Snippet} = require("../models");
 const { signToken, AuthenticationError } = require("../utils/auth");
 
 
 //testing resolvers for User, Snippet, Comment, and CodeBlock still a WIP playing around with it
 
-const resolvers = {
-  Query: {
-    //user query (still unsure if this is correct)
-    getUser: async (_, { username }) => {
-      try {
-        const user = await User.findOne({ username });
-        return user;
-      } catch (error) {
+const resolvers =
+{
+  Query:
+  {
+    allSnippets: async () =>
+    {
+      try
+      {
+        const snippets = await Snippet.find({});
+        return snippets;
+      }
+      catch (error)
+      {
         console.error(error);
-        throw new Error('Failed to fetch user');
+        throw new Error('Failed to retrieve snippets;', error);
       }
     },
-    //snippet query
-    getSnippet: async (_, { id }) => {
-      try {
-        const snippet = await Snippet.findById(id);
+    oneSnippet: async (parent, {snippetId}) =>
+    {
+      try
+      {
+        const snippet = await Snippet.find({_id: snippetId});
         return snippet;
-      } catch (error) {
+      }
+      catch (error)
+      {
         console.error(error);
-        throw new Error('Failed to fetch snippet');
+        throw new Error("Failed to retrieve the user's snippets;", error);
       }
     },
-    //comment query
-    getComment: async (_, { id }) => {
-      try {
-        const comment = await Comment.findById(id);
-        return comment;
-      } catch (error) {
+    userSnippets: async (parent, {username}) =>
+    {
+      try
+      {
+        const snippets = await Snippet.find({username});
+        return snippets;
+      }
+      catch (error)
+      {
         console.error(error);
-        throw new Error('Failed to fetch comment');
+        throw new Error("Failed to retrieve the user's snippets;", error);
       }
     },
-    //codeblock query
-    getCodeBlock: async (_, { id }) => {
-      try{
-        const codeBlock = await codeBlock.findById(id);
-        return codeBlock;
-      } catch (error) {
-        console.error(error);
-        throw new Error('Failed to fetch code block');
-      }
-  },
+    // user: async (parent, {username}) =>
+    // {
+    //   try
+    //   {
+    //     const user = await User.findOne({username});
+    //     return user;
+    //   }
+    //   catch (error)
+    //   {
+    //     console.error(error);
+    //     throw new Error('Failed to retrieve user;', error);
+    //   }
+    // },
   },
 
-//Might need to add more Queries still a WIP but should be a good start
+  // //Might need to add more Queries still a WIP but should be a good start
 
-  Mutation: {
-    createUser: async (_, { username, email, password }) => {
-      try {
-        const newUser = new User({ username, email, password });
-        const result = await newUser.save();
-        return result;
-      } catch (error) {
-        console.error(error);
-        throw new Error('Failed to create user');
-      }
-    },
-    createSnippet: async (_, { username, snippetText }) => {
-      try {
-        const newSnippet = new Snippet({ username, snippetText });
-        const result = await newSnippet.save();
-        return result;
-      } catch (error) {
-        console.error(error);
-        throw new Error('Failed to create snippet');
-      }
-    },
-    createComment: async (_, { username, commentText, snippetId }) => {
-      try {
-        const newComment = new Comment({ username, commentText });
-        const result = await newComment.save();
-      //store comments in array of comment IDs
-      const updatedSnippet = await Snippet.findOneAndUpdate(
-        snippetId,
-        { $push: { comments: result._id } },
-        { new: true }
-      );
-      return result;
-      }catch (error);
-      throw new Error('Failed to create comment');
-
-    },
-     
-    //resolver to handle upvotes and downvotes (still a WIP)
-    voteSnippet: async (_, { snippetId, voteType }) => {
-      try{
-        const updateField = voteType === 'props' ? 'rating.props' : 'rating.drops';
-
-        const updatedSnippet = await Snippet.findByIdAndUpdate(
-          snippetId, 
-          { $inc: { [updateField]: 1 } },
-          { new: true }
-        )
-      };
-      return updatedSnippet;
-    }catch (error) {
-      console.error(error);
-      throw new Error('Failed to vote on snippet');
-    },
-
-    createCodeBlock: async (_, { username, codeBlockText }) => {
-      try {
-        const newCodeBlock = new CodeBlock({ username, codeBlockText });
-        const result = await newCodeBlock.save();
-        return result;
-      } catch (error) {
-        console.error(error);
-        throw new Error('Failed to create code block');
-      }
-    }
-    // Need to add more mutations (WIP) this is gonna be LENGTHY hehe
-  },
-  User: {
-    snippets: async (parent) => {
-      try {
-        const user = await User.findById(parent.id).populate('snippets');
-        return user.snippets || [];
-      } catch (error) {
-        console.error(error);
-        throw new Error('Failed to fetch user snippets');
-      }
-    },
- 
-    // Need to add more field resolvers for the User type (WIP)
-  },
-  Snippet: {
-    comments: async (parent) => {
-      try {
-        const snippet = await Snippet.findById(parent.id).populate('comments');
-        return snippet.comments || [];
-      } catch (error) {
-        console.error(error);
-        throw new Error('Failed to fetch snippet comments');
-      }
-    },
-    // Need to add more field resolvers for the Snippet type and a bunch of crap HAHA UGHH (WIP)
-  },
+  //   Mutation:
+  //   {
+  //     createUser: async (_, { username, email, password }) =>
+  //     {
+  //       try 
+  //       {
+  //         const newUser = new User({ username, email, password });
+  //         const result = await newUser.save();
+  //         return result;
+  //       }
+  //       catch (error)
+  //       {
+  //         console.error(error);
+  //         throw new Error('Failed to create user');
+  //       }
+  //     },
+  //     createSnippet: async (_, { username, snippetText }) =>
+  //     {
+  //       try
+  //       {
+  //         const newSnippet = new Snippet({ username, snippetText });
+  //         const result = await newSnippet.save();
+  //         return result;
+  //       }
+  //       catch (error)
+  //       {
+  //         console.error(error);
+  //         throw new Error('Failed to create snippet');
+  //       }
+  //     },
+  //     createComment: async (_, { username, commentText, snippetId }) =>
+  //     {
+  //       try
+  //       {
+  //         const newComment = new Comment({ username, commentText });
+  //         const result = await newComment.save();
+  //       //store comments in array of comment IDs
+  //       const updatedSnippet = await Snippet.findOneAndUpdate(
+  //         snippetId,
+  //         { $push: { comments: result._id } },
+  //         { new: true }
+  //       );
+  //       return result;
+  //       }
+  //       catch (error)
+  //       {
+  //         throw new Error('Failed to create comment');
+  //       }
+  //     },
+  //     createCodeBlock: async (_, { username, codeBlockText }) => {
+  //       try {
+  //         const newCodeBlock = new CodeBlock({ username, codeBlockText });
+  //         const result = await newCodeBlock.save();
+  //         return result;
+  //       } catch (error) {
+  //         console.error(error);
+  //         throw new Error('Failed to create code block');
+  //       }
+  //     }
+  //     // Need to add more mutations (WIP) this is gonna be LENGTHY hehe
+  //   },
+  //   User: {
+  //     snippets: async (parent) => {
+  //       try {
+  //         const user = await User.findById(parent.id).populate('snippets');
+  //         return user.snippets || [];
+  //       } catch (error) {
+  //         console.error(error);
+  //         throw new Error('Failed to fetch user snippets');
+  //       }
+  //     },
+  
+  //     // Need to add more field resolvers for the User type (WIP)
+  //   },
+  //   Snippet: {
+  //     comments: async (parent) => {
+  //       try {
+  //         const snippet = await Snippet.findById(parent.id).populate('comments');
+  //         return snippet.comments || [];
+  //       } catch (error) {
+  //         console.error(error);
+  //         throw new Error('Failed to fetch snippet comments');
+  //       }
+  //     },
+  //     // Need to add more field resolvers for the Snippet type and a bunch of crap HAHA UGHH (WIP)
+  //   },
 };
 
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -7,63 +7,71 @@
 // `;
 
 const typeDefs = `
-type User {
+type CodeBlock
+{
+  _id: ID!
+  language: String!
+  code: String!
+}
+
+type Comment
+{
+  _id: ID!
+  username: String!
+  commentText: String!
+  commentCode: [CodeBlock]
+  creationDate: String!
+  formattedCreationDate: String!
+}
+
+type Snippet
+{
+  _id: ID!
+  username: String!
+  snippetTitle: String!
+  snippetText: String!
+  snippetCode: [CodeBlock]!
+  creationDate: String!
+  editDate: String
+  comments: [Comment]
+  props: [ID]
+  drops: [ID]
+  overallProps: Int
+  formattedCreationDate: String!
+  formattedEditDate: String
+}
+
+type Query
+{
+  allSnippets: [Snippet]
+  oneSnippet(snippetId: ID!): [Snippet]
+  userSnippets(username: String!): [Snippet]
+}
+`;
+
+/*
+type User
+{
   id: ID!
   username: String!
   email: String!
-  snippets: [Snippet]
-  # need to add more fields WIP
+  snippets: [Snippet]!
 }
 
-type Snippet {
-  id: ID!
-  username: String!
-  snippetText: String!
-  comments: [Comment]
-  rating: [Ratings]
-  # need to add more fields WIP
-}
-
-type Comment {
-  id: ID!
-  username: String!
-  commentText: String!
-  # need to add more fields WIP
-}
-
-type CodeBlock {
-  id: ID!
-  username: String!
-  codeBlockText: String!
-  # need to add more fields WIP
-}
-
-type Ratings {
-  props: Int!
-  drops: Int!
-}
-
-
-type Query {
-  getUser(username: String!): User
   getSnippet(id: ID!): Snippet
   getComment(id: ID!): Comment
   getCodeBlock(id: ID!): CodeBlock
   # need to add more fields WIP
 }
+ */
 
-type Mutation {
+/*type Mutation {
   createUser(username: String!, email: String!, password: String!): User
   createSnippet(username: String!, snippetText: String!): Snippet
   createComment(username: String!, commentText: String!): Comment
   createCodeBlock(username: String!, codeBlockText: String!): CodeBlock
   # need to add more fields WIP
-}
-`;
-
-
-
-
+} */
 
 
 
