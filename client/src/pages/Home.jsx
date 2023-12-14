6ad188394e040b271f6af4a28a559c73954e59d7
Carlos Macias <cimm21187@gmail.com>
2023-12-14 18:56:55 -0500

    style Home component

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
-import { Box, Text } from '@chakra-ui/react';
-import theme from "../utils/theme";
+import { Box, Button, Text, VStack, Heading, SimpleGrid, Image } from '@chakra-ui/react';
 
-export default function Home () {
-    return (
-        <Box>
-            <Box>
-                <Text color='codex.accents'>Hey There!</Text>
-            </Box>
-        </Box>
-    );
+function Home() {
+  return (
+    <Box textAlign="center" fontSize="xl">
+      <VStack spacing={8}>
+        <Heading as="h1" size="2xl" my={6}>
+          Cool Title of our app
+        </Heading>
+        <Text>Cool Description of our app</Text>
+        <Button>
+          Cool button
+        </Button>
+        <SimpleGrid columns={{ base: 1, md: 3 }} spacing={5} mt={8}>
+          <Image boxSize="550px" src="/images/home/code01.png" alt="Graphic 1" />
+          <Image boxSize="550px" src="/images/home/code02.png" alt="Graphic 2" />
+          <Image boxSize="550px" src="/images/home/code03.png" alt="Graphic 3" />
+        </SimpleGrid>
+      </VStack>
+    </Box>
+  );
 }
+
+export default Home;
