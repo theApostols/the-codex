3cccbcf81fb5d6bd91efd9a87e1e5d9cc8be6ccb
Wes <wesley.has@outlook.com>
2023-12-13 18:11:31 -0700

    added login/signup redirect

--- client/src/components/Login-Signup/LoginForm.jsx
+++ client/src/components/Login-Signup/LoginForm.jsx
@@ -1,20 +1,40 @@
-import React from 'react';
-import { Box, VStack, Heading, Input, Button, Text, Link } from '@chakra-ui/react';
+import React from "react";
+import {
+  Box,
+  VStack,
+  Heading,
+  Input,
+  Button,
+  Text,
+  Link,
+} from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
 
 function LoginForm() {
+  const navigate = useNavigate();
+
+  const handleSignUpClick = () => {
+    // Navigate to the sign-up page
+    navigate("/signup");
+  };
+
   return (
-      <VStack p='50px' spacing={4} align='stretch'>
-        <Heading>Login</Heading>
-        <Input placeholder="username" />
-        <Input placeholder="password" type="password" />
-        <Button colorScheme="blue">Login</Button>
-        <Text>
-          Don't have an account? 
-          <Link color='teal.500' onClick={() => navigate('/signup')} style={{ cursor: 'pointer' }}>
-            <Button variant='link'>Signup</Button>
-          </Link>
-        </Text>
-      </VStack>
+    <VStack p="50px" spacing={4} align="stretch">
+      <Heading>Login</Heading>
+      <Input placeholder="username" />
+      <Input placeholder="password" type="password" />
+      <Button colorScheme="blue">Login</Button>
+      <Text>
+        Don't have an account?
+        <Link
+          color="teal.500"
+          onClick={handleSignUpClick}
+          style={{ cursor: "pointer" }}
+        >
+          <Button variant="link">Signup</Button>
+        </Link>
+      </Text>
+    </VStack>
   );
 }
 
