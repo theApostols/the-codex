b779e8b6c084338dcb09763a4f92ebd1c19afa04
ethan <hertlethan@gmail.com>
2023-12-19 14:37:34 -0500

    added snippet preview component & page to view all snippets from a specific user

---
+++ client/src/pages/UserSnippets.jsx
@@ -0,0 +1,37 @@
+import { useQuery, useMutation } from "@apollo/client";
+import { useParams } from 'react-router-dom';
+import React from "react";
+import SnippetPreview from "../components/Snippet/SnippetPreview.jsx";
+import { GET_USER_SNIPPETS } from "../utils/queries";
+
+export default function UserSnippets() {
+  const paragraphStyle = {
+    fontSize: "18px",
+    color: "white",
+    fontWeight: "bold",
+  };
+
+  //retrieve user route parameter
+  const {username} = useParams();
+
+  // Use the useQuery hook to execute the GET_USER_SNIPPETS query
+  const { loading, error, data } = useQuery(GET_USER_SNIPPETS,
+  {
+    variables: {username}
+  });
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
+  // Extract snippets from the data
+  const snippets = data.userSnippets;
+
+  return (
+    <>
+      <p style={paragraphStyle}>UserPage is rendering</p>
+      {snippets.map((snippet) => (
+        <SnippetPreview key={snippet._id} snippet={snippet}/>
+      ))}
+    </>
+  );
+}
