17cd1b554609fe403a4fab5402bb284eb961f693
Stavros Panagiotopoulos <stavros_pana@hotmail.com>
2023-12-18 19:50:22 -0500

    updated personaldashboard, added some stuff. WIP

--- client/src/pages/PersonalDashboard.jsx
+++ client/src/pages/PersonalDashboard.jsx
@@ -1,35 +1,60 @@
-// import React, { useState, useEffect } from 'react';
-// import SnippetCard from './SnippetCard';
-// import { User, Snippet } from '../utils/API';
-// import { getUserSnippets } from '../utils/API/userAPI';
-// // // need to fix imports 
+import React, { useState, useEffect } from 'react';
+import { useQuery } from '@apollo/client';
+import { GET_USER_PROFILE } from '../utils/queries'; 
+import SnippetCard from '../components/SnippetCard'; 
+import CommentCard from '../components/CommentCard'; 
 
+    const PersonalDashboard = () => {
+        const [userProfile, setUserProfile] = useState(null);
+      
+        const { loading, error, data } = useQuery(GET_USER_PROFILE);
+      
+        useEffect(() => {
+          if (data && data.userProfile) {
+            setUserProfile(data.userProfile);
+          }
+        }, [data]);
+      
+        if (loading) {
+          return <p>Loading...</p>;
+        }
+      
+        if (error) {
+          console.error('Error fetching user profile:', error);
+          return <p>Error fetching user profile</p>;
+        }
+      
+        return (
+          <div>
+            {userProfile && (
+              <>
+                <h1>Welcome, {userProfile.username}!</h1>
+                {/* Display user information */}
+                <div>
+                  <h2>Your Snippets</h2>
+                  {userProfile.snippets.map((snippet) => (
+                    <SnippetCard key={snippet.id} snippet={snippet} />
+                  ))}
+                </div>
+                <div>
+                  <h2>Your Comments</h2>
+                  {userProfile.comments.map((comment) => (
+                    <CommentCard key={comment.id} comment={comment} />
+                  ))}
+                </div>
+                    <div>
+                    <h2>Saved Snippets</h2>
+                    {userProfile.savedsnippets.map((savedsnippets) => (
+                        <SavedSnippets key={savedsnippets.id} />
+                    ))}
+                    </div>
+                    <div>
 
-const PersonalDashboard = ({ userID }) => {
-    const [snippets, setSnippets] = useState([]);
-
-    useEffect(() => {
-        //fetches the users snippets when the component mounts
-        const fetchUserSnippets = async () => {
-            try {
-                const snippetsData = await User.getUserSnippets(userID);
-                setSnippets(snippetsData);
-            } catch (error) {
-                console.log('error fetching user snippets', error);
-            }
-        };
-        fetchUserSnippets();
-    }, [userID]);
-    return (
-        <div>
-            <h1>Personal Dashboard</h1>
-            <div>
-                {snippets.map((snippet) => (
-                    <SnippetCard key={snippet._id} snippet={snippet} />
-                ))}
-            </div>
-        </div>
-    );
- };
-
-    export default PersonalDashboard;
+                    </div>
+              </>
+            )}
+          </div>
+        );
+      };
+      
+      export default PersonalDashboard;
