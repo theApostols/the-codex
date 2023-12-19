a73673c334222da5cd605f128e6309e9c0d92c83
Wes <wesley.has@outlook.com>
2023-12-19 13:51:49 -0700

    added redirect function for create on drawer and also to close drawer once redirected

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -25,12 +25,17 @@ import {
   Icon,
 } from "@chakra-ui/react";
 import { BiLogOut, BiMenu, BiPlus, BiUser, BiCog } from "react-icons/bi";
-import AuthService from '../../utils/auth';
+import AuthService from "../../utils/auth";
 
 const Header = () => {
   const navigate = useNavigate();
   const isAuthenticated = AuthService.loggedIn();
   const user = isAuthenticated ? AuthService.getProfile() : null;
+
+  const handleCreateClick = () => {
+    navigate("/createsnippet");
+    onClose();
+  };
 
   const handleSignUpClick = () => {
     navigate("/signup");
@@ -148,6 +153,7 @@ const Header = () => {
                 }
                 variant="ghost"
                 justifyContent="start"
+                onClick={handleCreateClick}
               >
                 Create
               </Button>
