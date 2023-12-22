7969c7055f45d6c4f1eaaa8b1c92c06c89cbcaa4
ethan <hertlethan@gmail.com>
2023-12-21 22:07:38 -0500

    updated clientside queries to reflect split user snippet queries

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,6 +1,6 @@
 import { gql } from "@apollo/client";
 
-//OLD QUERY GET_ALL_USERS
+// OLD QUERIES
 // export const GET_ALL_USERS = gql`
 //   query AllUsers {
 //     allUsers {
@@ -26,6 +26,68 @@ import { gql } from "@apollo/client";
 //           formattedEditDate
 //         }
 //         overallProps
+//       }
+//     }
+//   }
+// `;
+// export const GET_USER_SNIPPETS = gql`
+//   query UserSnippets($username: String!, $tags: [String]) {
+//     userSnippets(username: $username, tags: $tags) {
+//       _id
+//       username
+//       snippetTitle
+//       snippetText
+//       snippetCode {
+//         code
+//         language
+//       }
+//       tags
+//       overallProps
+//       formattedCreationDate
+//       formattedEditDate
+//     }
+//   }
+// `;
+
+// export const GET_ALL_SNIPPETS = gql`
+//   query Query {
+//     allSnippets {
+//       _id
+//       snippetText
+//       snippetCode {
+//         code
+//         language
+//       }
+//       formattedCreationDate
+//       formattedEditDate
+//       props
+//       drops
+//     }
+//   }
+// `;
+
+// export const GET_INDIVIDUAL_SNIPPET = gql`
+//   query Query($snippetId: ID!) {
+//     oneSnippet(snippetId: $snippetId) {
+//       snippetText
+//       snippetTitle
+//       snippetCode {
+//         code
+//         language
+//       }
+//       creationDate
+//       editDate
+//       formattedCreationDate
+//       formattedEditDate
+//       props
+//       drops
+//       overallProps
+//       tags
+//       comments {
+//         username
+//         commentText
+//         formattedCreationDate
+//         formattedEditDate
 //       }
 //     }
 //   }
@@ -86,17 +148,37 @@ query AllSnippets {
   }
 }`;
 
-//query to retrieve a specific user's snippets by username, filtering by tags
+//query to retrieve a specific user's snippets by username, filtering by tags, plus their username & image
 //used on the user snippets page
-//used on the 'my snippets' page of the personal dashboard
 //retrieves all snippet data necessary to render each snippet preview
-//username argument is retrieved from;
-  //on user-snippets page - route parameter
-  //on personal dashboard 'my snippets' page - JWT
+//username argument is retrieved from route parameter
 //tags argument is provided by checkboxes on the page (provide 'null' if no checkboxes are ticked)
 export const GET_USER_SNIPPETS = gql`
 query UserSnippets($username: String!, $tags: [String]) {
   userSnippets(username: $username, tags: $tags) {
+    user {
+      username
+      image
+    }
+    snippets {
+      _id
+      username
+      snippetTitle
+      snippetText
+      overallProps
+      formattedCreationDate
+      formattedEditDate
+    }
+  }
+}`;
+
+//query to retrieve your snippets by your username
+//used on the 'my snippets' page of the personal dashboard
+//retrieves all snippet data necessary to render each snippet preview
+//username argument is retrieved from JWT
+export const GET_MY_SNIPPETS = gql`
+query MySnippets($username: String!) {
+  mySnippets(username: $username) {
     _id
     username
     snippetTitle
@@ -151,11 +233,7 @@ query UserComments($username: String!) {
 //query to retrieve a specific snippet's full data by ObjectId
 //used on an individual snippet's page (i.e. after clicking a snippet preview & being routed to that snippet's dedicated page)
 //retrieves all snippet data, including code blocks & resources, plus all comments & comment data
-//I DON'T KNOW WHERE THE SNIPPETID WILL COME FROM, SINCE THIS HASN'T BEEN IMPLEMENTED YET, BUT HERE IS AN IDEA
-  //snippetId argument is retrieved from the key value of the snippet preview that was clicked
-    //whenever a snippet preview is generated, assign the snippet's ObjectId as a 'key' attribute, and retrieve this to use as the argument
-  //snippetId argument is retrieved from route parameters
-    //each snippet preview is a link to a dedicated page using the snippet's ObjectId as part of the href; extract the Id to use as the argument
+//snippetId argument is retrieved from route parameters
 export const GET_INDIVIDUAL_SNIPPET = gql`
 query OneSnippet($snippetId: ID!) {
   oneSnippet(snippetId: $snippetId) {
