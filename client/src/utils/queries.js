8b53dda2d9def69ec3299d6f3b9b2cd73c50bbf9
ethan <hertlethan@gmail.com>
2023-12-21 19:45:20 -0500

    updated client util queries to be in-line with backend resolvers & typedefs

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -59,73 +59,128 @@ export const GET_ALL_USERS = gql`
   }
 `;
 
+//query to one user's data by username
+//used on the user settings page
+//retrieves the 'image' value of a specific user to render to the profile image preview as part of the 'src' attribute
+//username argument is retrieved from JWT
 export const GET_ONE_USER = gql`
-  query oneUser($username: String!) {
-    oneUser(username: $username) {
-      image
-    }
+query oneUser($username: String!) {
+  oneUser(username: $username) {
+    image
   }
-`;
+}`;
 
+//query to retrieve all snippet data
+//used on the main snippets page
+//retrieves all snippet data necessary to render each snippet preview
+export const GET_ALL_SNIPPETS = gql`
+query AllSnippets {
+  allSnippets {
+    _id
+    username
+    snippetTitle
+    snippetText
+    overallProps
+    formattedCreationDate
+    formattedEditDate
+  }
+}`;
+
+//query to retrieve a specific user's snippets by username, filtering by tags
+//used on the user snippets page
+//used on the 'my snippets' page of the personal dashboard
+//retrieves all snippet data necessary to render each snippet preview
+//username argument is retrieved from;
+  //on user-snippets page - route parameter
+  //on personal dashboard 'my snippets' page - JWT
+//tags argument is provided by checkboxes on the page (provide 'null' if no checkboxes are ticked)
 export const GET_USER_SNIPPETS = gql`
-  query UserSnippets($username: String!, $tags: [String]) {
-    userSnippets(username: $username, tags: $tags) {
+query UserSnippets($username: String!, $tags: [String]) {
+  userSnippets(username: $username, tags: $tags) {
+    _id
+    username
+    snippetTitle
+    snippetText
+    overallProps
+    formattedCreationDate
+    formattedEditDate
+  }
+}`;
+
+//query to retrieve a specific user's saved snippets by username
+//used on the 'saved snippets' tab of the personal dashboard
+//retrieves all snippet data necessary to render each snippet preview
+//username argument is retrieved from JWT
+export const GET_SAVED_SNIPPETS = gql`
+query UserSavedSnippets($username: String!) {
+  userSavedSnippets(username: $username) {
+    savedSnippets
+    {
       _id
       username
       snippetTitle
       snippetText
-      snippetCode {
-        code
-        language
-      }
-      tags
       overallProps
       formattedCreationDate
       formattedEditDate
     }
   }
-`;
+}`;
 
-export const GET_ALL_SNIPPETS = gql`
-  query Query {
-    allSnippets {
+//query to retrieve a specific user's comments by username
+//used on the 'my comments' tab of the personal dashboard
+//retrieves all comment data, and parent snippet data necessary to render a small preview
+//username argument is retrieved from JWT
+export const GET_USER_COMMENTS = gql`
+query UserComments($username: String!) {
+  userComments(username: $username) {
+    username
+    commentText
+    formattedCreationDate
+    formattedEditDate
+    parentSnippetId {
       _id
-      snippetText
-      snippetCode {
-        code
-        language
-      }
-      formattedCreationDate
-      formattedEditDate
-      props
-      drops
-    }
-  }
-`;
-
-export const GET_INDIVIDUAL_SNIPPET = gql`
-  query Query($snippetId: ID!) {
-    oneSnippet(snippetId: $snippetId) {
-      snippetText
+      username
       snippetTitle
-      snippetCode {
-        code
-        language
-      }
-      creationDate
-      editDate
       formattedCreationDate
       formattedEditDate
-      props
-      drops
-      overallProps
-      tags
-      comments {
-        username
-        commentText
-        formattedCreationDate
-        formattedEditDate
-      }
     }
   }
-`;
+}`;
+
+//query to retrieve a specific snippet's full data by ObjectId
+//used on an individual snippet's page (i.e. after clicking a snippet preview & being routed to that snippet's dedicated page)
+//retrieves all snippet data, including code blocks & resources, plus all comments & comment data
+//I DON'T KNOW WHERE THE SNIPPETID WILL COME FROM, SINCE THIS HASN'T BEEN IMPLEMENTED YET, BUT HERE IS AN IDEA
+  //snippetId argument is retrieved from the key value of the snippet preview that was clicked
+    //whenever a snippet preview is generated, assign the snippet's ObjectId as a 'key' attribute, and retrieve this to use as the argument
+  //snippetId argument is retrieved from route parameters
+    //each snippet preview is a link to a dedicated page using the snippet's ObjectId as part of the href; extract the Id to use as the argument
+export const GET_INDIVIDUAL_SNIPPET = gql`
+query OneSnippet($snippetId: ID!) {
+  oneSnippet(snippetId: $snippetId) {
+    _id
+    username
+    snippetTitle
+    snippetText
+    snippetCode {
+      language
+      code
+    }
+    comments {
+      _id
+      username
+      commentText
+      formattedCreationDate
+      formattedEditDate
+    }
+    resources {
+      title
+      link
+    }
+    tags
+    overallProps
+    formattedCreationDate
+    formattedEditDate
+  }
+}`;
